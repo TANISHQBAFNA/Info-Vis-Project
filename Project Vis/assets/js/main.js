64b63fe9ba9105abe36b21f41d500e80e378e44f
Tanishq Bafna <38152364+TANISHQBAFNA@users.noreply.github.com>
2022-04-10 15:27:09 -0400

    Add files via upload

---
+++ Project Vis/assets/js/main.js
@@ -0,0 +1,155 @@
+currentWidth = parseInt(d3.select('#circular-chart').style('width'), 10)
+currentheight = parseInt(d3.select('.circular-chart').style('height'), 10)
+// set the dimensions and margins of the graph
+
+
+var margin = {top: 0, right: 0, bottom: 0, left: 0},
+  width = currentWidth ,
+  height = currentheight ,
+  innerRadius = 40,
+  outerRadius = Math.min(width, height) / 2;   // the outerRadius goes from the middle of the SVG area to the border
+
+// append the svg object to the body of the page
+var svg = d3.select("body").select(".circular-chart").select(".row").select("#circular-chart")
+  .append("svg")
+  .attr("width", width )
+  .attr("height", height )
+  .append("g")
+  .attr("transform", "translate(" + width / 2 + "," + ( height/2+100 )+ ")")// Add 100 on Y translation, cause upper bars are longer
+
+d3.csv("assets/data/SVICounty.csv", function(data) {
+
+  // X scale
+  // var color = d3.scale.ordinal()
+  //   .range(["#EDC951","#CC333F","#00A0B0"]);
+
+  var x = d3.scaleBand()
+    .range([0, 2 * Math.PI])    // X axis goes from 0 to 2pi = all around the circle. If I stop at 1Pi, it will be around a half circle
+    .align(0)                  // This does nothing ?
+    .domain( data.map(function(d) { return d.COUNTY; }) ); // The domain of the X axis is the list of states.
+
+  // Y scale
+  var y = d3.scaleRadial()
+    .range([innerRadius, outerRadius])   // Domain will be define later.
+    .domain([0, 1]); // Domain of Y is from 0 to the max seen in the data
+
+  // Add bars
+  svg.append("g")
+    .selectAll("path")
+    .data(data)
+    .enter()
+    .append("path")
+    .attr("fill", function (d){
+      if(d.THEMES>0.75){
+        return "#ff3e50";
+      }
+      if(d.THEMES<=0.75 && d.THEMES>0.5 ){
+        return "#EDC951";
+      }
+      if(d.THEMES<=0.5 && d.THEMES>0.25){
+        return "#00A0B0";
+      }
+      if(d.THEMES<=0.25){
+        return "#00b052";
+      }
+    })
+    .attr("d", d3.arc()     // imagine your doing a part of a donut plot
+      .innerRadius(innerRadius)
+      .outerRadius(function(d) { return y(d['THEMES']); })
+      .startAngle(function(d) { return x(d.COUNTY); })
+      .endAngle(function(d) { return x(d.COUNTY) + x.bandwidth(); })
+      .padAngle(0.01)
+      .padRadius(innerRadius))
+    // .on("mouseover", function(d) {
+    //   //Bring back the hovered over blob
+
+    // })
+    // .on('mouseout', function(){
+    //   //Bring back all blobs
+
+    // })
+    .on('mouseover', mouseover)
+    .on('mousemove', mousemove)
+    .on('mouseout', mouseout)
+    .on('click', function (d){
+ d3.csv("assets/data/County Info.csv", function(info) {
+   let ind;
+   console.log(info)
+   for(let i=0;i<info.length;i++){
+     if(d.COUNTY === info[i].County){
+       ind=i;
+     }
+   }
+   // low level of vulnerability low to moderate level of vulnerability  a moderate to high level of vulnerability high level of vulnerability.
+
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-name').text(info[ind].County)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-est').text(info[ind].Est)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-origin').text(info[ind].Origin)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-eth').text(info[ind].Etymology)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-area').text(info[ind].Area)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-population').text(info[ind].Population)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').select('.buttons').select('a').text("Explore SVI data for "+ info[ind].County)
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').select('h5').select('#county-vulnerability').text(function (){
+     if(d.THEMES>0.75){
+       return "High level of vulnerability";
+     }
+     if(d.THEMES<=0.75 && d.THEMES>0.5 ){
+       return "Moderate to high level of vulnerability";
+     }
+     if(d.THEMES<=0.5 && d.THEMES>0.25){
+       return "Low to moderate level of vulnerability";
+     }
+     if(d.THEMES<=0.25){
+       return "Low level of vulnerability";
+     }
+     d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').append("button")
+
+     })
+   d3.select(".circular-chart").select(".row").select("#circular-info").select('.intro').select('div').selectAll('div').selectAll('h5').select('span')
+     .style('color', function (){
+       if(d.THEMES>0.75){
+         return "#ff3e50";
+       }
+       if(d.THEMES<=0.75 && d.THEMES>0.5 ){
+         return "#EDC951";
+       }
+       if(d.THEMES<=0.5 && d.THEMES>0.25){
+         return "#00A0B0";
+       }
+       if(d.THEMES<=0.25){
+         return "#00b052";
+       }
+     });
+    })
+      radar(d.COUNTY)
+        .on('mouseover', mouseover)
+        .on('mousemove', mousemove)
+        .on('mouseout', mouseout);
+    });
+
+
+  var div = d3.select(".circular-chart").select(".row").select("#circular-chart").append('div')
+    .attr('class', 'tooltip2')
+    .style('display', 'none');
+  function mouseover(){
+    div.style('display', 'inline');
+    d3.select(this)
+      .transition().duration(200)
+      .style("fill-opacity", 0.5)
+  }
+  function mousemove(){
+    var d = d3.select(this).data()[0]
+    div
+      .html("County: " + d.COUNTY + '<hr/>' + "SVI: " + d.THEMES)
+      .style('left', (d3.event.pageX - 24) + 'px')
+      .style('top', (d3.event.pageY - 10) + 'px');
+  }
+  function mouseout(){
+    div.style('display', 'none');
+    d3.select(this)
+      .transition().duration(200)
+      .style("fill-opacity", 1)
+
+  }
+
+});
